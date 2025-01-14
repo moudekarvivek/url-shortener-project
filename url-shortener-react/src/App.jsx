6481735cc8517fbcdf3e5f9b02fb20ac87482482
moudekarvivek <moudekarvivek@gmail.com>
2025-01-14 17:44:50 +0530

    Making API calls to the backend using CORS and used ReactToast to Notify the registration popup

--- url-shortener-react/src/App.jsx
+++ url-shortener-react/src/App.jsx
@@ -5,11 +5,13 @@ import AboutPage from './components/AboutPage'
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 import Navbar from './components/NavBar'
 import RegisterPage from './components/RegisterPage'
+import { Toaster } from 'react-hot-toast'
 
 function App() {
   return (
     <Router>
-    <Navbar />
+      <Navbar />
+      <Toaster position='bottom-center'/>
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/about" element={<AboutPage />} />
--- url-shortener-react/src/components/RegisterPage.jsx
+++ url-shortener-react/src/components/RegisterPage.jsx
@@ -1,9 +1,14 @@
 import React, { useState } from 'react'
 import { useForm } from 'react-hook-form'
 import TextField from './TextField';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import api from '../api/api';
+import toast from 'react-hot-toast';
+
 const RegisterPage = () => {
+    const navigate = useNavigate();
     const [loader, setLoader] = useState(false);
+
     const {
         register,
         handleSubmit,
@@ -17,9 +22,25 @@ const RegisterPage = () => {
         },
         mode: "onTouched",
     });
+
     const registerHandler = async (data) => {
-        
+        setLoader(true);
+        try {
+            const { data: response } = await api.post(
+                "/api/auth/public/register",
+                data
+            );
+            reset();
+            navigate("/login");
+            toast.success("Registeration Successful!")
+        } catch (error) {
+            console.log(error);
+            toast.error("Registeration Failed!")
+        } finally {
+            setLoader(false);
+        }
     };
+
   return (
     <div
         className='min-h-[calc(100vh-64px)] flex justify-center items-center'>
@@ -28,7 +49,9 @@ const RegisterPage = () => {
             <h1 className="text-center font-serif text-btnColor font-bold lg:text-3xl text-2xl">
                 Register Here
             </h1>
+
             <hr className='mt-2 mb-5 text-black'/>
+
             <div className="flex flex-col gap-3">
                 <TextField
                     label="UserName"
@@ -40,6 +63,7 @@ const RegisterPage = () => {
                     register={register}
                     errors={errors}
                 />
+
                 <TextField
                     label="Email"
                     required
@@ -50,6 +74,7 @@ const RegisterPage = () => {
                     register={register}
                     errors={errors}
                 />
+
                 <TextField
                     label="Password"
                     required
@@ -62,22 +87,25 @@ const RegisterPage = () => {
                     errors={errors}
                 />
             </div>
+
             <button
                 disabled={loader}
                 type='submit'
                 className='bg-customRed font-semibold text-white  bg-custom-gradient w-full py-2 hover:text-slate-400 transition-colors duration-100 rounded-sm my-3'>
                 {loader ? "Loading..." : "Register"}
             </button>
+
             <p className='text-center text-sm text-slate-700 mt-6'>
                 Already have an account? 
                 <Link
                     className='font-semibold underline hover:text-black'
                     to="/login">
-                        <span className='text-btnColor'>Login</span>
+                        <span className='text-btnColor'> Login</span>
                 </Link>
             </p>
         </form>
     </div>
   )
 }
+
 export default RegisterPage
